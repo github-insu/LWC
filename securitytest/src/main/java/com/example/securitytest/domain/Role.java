3d733ec2ae2c84bfa67637c13958d3e33fbff1a5
github-insu <93insu@gmail.com>
2023-10-17 15:45:39 +0900

    Fix : misupload
    Explain : misupload

--- securitytest/src/main/java/com/example/securitytest/domain/Role.java
+++
@@ -1,7 +0,0 @@
-package com.example.securitytest.domain;
-
-import java.io.Serializable;
-
-public enum Role implements Serializable {
-    ROLE_ADMIN, ROLE_MANAGER, ROLE_MEMBER
-}
--- securitytest/src/main/java/com/example/securitytest/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.securitytest.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends CrudRepository<Member,String>{
-}
